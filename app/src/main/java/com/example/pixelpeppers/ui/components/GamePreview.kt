3f493f2b30ce73c9b1c3cc4508ffb09dcf897d80
Itaykal <iykalfon@gmail.com>
2024-06-11 16:49:54 +0300

    LargeGamePreview component

--- app/src/main/java/com/example/pixelpeppers/ui/components/GamePreview.kt
+++ app/src/main/java/com/example/pixelpeppers/ui/components/GamePreview.kt
@@ -13,14 +13,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.bumptech.glide.integration.compose.CrossFade
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.integration.compose.placeholder
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.load.resource.bitmap.CenterInside
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.pixelpeppers.R
 
@@ -33,6 +34,8 @@ fun GamePreview(
     igdbArtworkId: String,
     description: String,
     modifier: Modifier = Modifier,
+    contentScale: ContentScale = ContentScale.Fit,
+    alignment: Alignment = Alignment.Center,
     titleOn: Boolean = true,
     title: String = "Placeholder",
     onClick: () -> Unit = {},
@@ -41,12 +44,15 @@ fun GamePreview(
         modifier = Modifier
             .border(width = 0.dp, color = Color.Transparent, shape = MaterialTheme.shapes.extraLarge)
             .padding(PaddingValues(bottom = 25.dp))
+            .clip(shape= MaterialTheme.shapes.extraLarge)
     ) {
         GlideImage(
             model = igdbImagePrefix + igdbArtworkId + igdbImageSuffix,
             contentDescription = description,
             loading = placeholder(R.drawable.avd_anim_placeholder),
             transition = CrossFade,
+            alignment = alignment,
+            contentScale = contentScale,
             modifier = modifier
                 .clickable(onClick = onClick)
                 .height(196.dp)
@@ -54,7 +60,7 @@ fun GamePreview(
         ) {
             it
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .transform(CenterInside(), RoundedCorners(70))
+                .transform(RoundedCorners(70))
         }
         if (titleOn) {
             Text(
--- app/src/main/java/com/example/pixelpeppers/ui/screens/TempPage.kt
+++ app/src/main/java/com/example/pixelpeppers/ui/screens/TempPage.kt
@@ -1,14 +1,31 @@
 package com.example.pixelpeppers.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.pixelpeppers.ui.components.SearchBar
+import com.example.pixelpeppers.models.Game
+import com.example.pixelpeppers.models.Genre
+import com.example.pixelpeppers.ui.components.LargeGamePreview
 
 @Composable
 fun TempPage(
     modifier: Modifier = Modifier,
 ) {
-    SearchBar()
+    Box(
+        modifier = modifier.background(MaterialTheme.colorScheme.background).fillMaxSize()
+    ) {
+        var g = Game(
+            id = 17000,
+            name = "Stardew Valley",
+            posterURL = "https://images.igdb.com/igdb/image/upload/t_cover_big/xrpmydnu9rpxvxfjkiu7.jpeg",
+            genres = listOf<Genre>(Genre("Indie"), Genre("Farming"))
+        )
+        LargeGamePreview(game = g, description = "")
+    }
+
 
 //    val artworks: List<String> = listOf<String>(
 //        "co2k2z", "co6xe3", "co5ff7", "co1nic",
